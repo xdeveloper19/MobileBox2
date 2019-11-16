04f4574d877939925aae3d75e29a04f5074dbedc
Ilya <50288548+daryalove@users.noreply.github.com>
2019-11-16 13:19:39 +0300

    second commit

---
+++ Container/SignalStrength.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//using Android.App;
+//using Android.Content;
+//using Android.OS;
+//using Android.Runtime;
+//using Android.Telephony;
+//using Android.Views;
+//using Android.Widget;
+
+//namespace GeoGeometry.Container
+//{
+//    class SignalStrength : PhoneStateListener
+//    {
+//        public delegate void SignalStrengthChangedDelegate(int strength);
+
+//        public event SignalStrengthChangedDelegate SignalStrengthChanged;
+
+//        public override void OnSignalStrengthsChanged(SignalStrength newSignalStrength)
+//        {
+//            if (newSignalStrength.IsGsm)
+//            {
+//                if (SignalStrengthChanged != null)
+//                {
+//                    SignalStrengthChanged(newSignalStrength.GsmSignalStrength);
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ Model/Box/SmartBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GeoGeometry.Model.Box
+{
+    public class SmartBox
+    {
+        public enum ContainerState
+        {
+            //сложенный, то есть контейнер закрыт
+
+            onBase = 1, //на складе
+            onCar,//на автомобиле
+            onShipper, //выгружен у грузоотправителя
+            onConsignee//разгружен у грузополучателя
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOpenedBox { get; set; }
+        public ContainerState BoxState { get; set; }
+        public bool IsOpenedDoor { get; set; }
+
+
+        public double Weight { get; set; }
+
+
+        public int Light { get; set; }
+        public string Code { get; set; }
+
+
+        public double Temperature { get; set; }
+
+
+        public double Wetness { get; set; }
+        public double BatteryPower { get; set; }
+    }
+}
