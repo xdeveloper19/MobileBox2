3e14ceaecea315ef36203693e6f399cde4b550c6
Darya Yakovleva <50288548+daryalove@users.noreply.github.com>
2019-12-08 11:20:59 +0300

    10 commit

---
+++ Model/GPSLocation/BoxLocation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GeoGeometry.Model.GPSLocation
+{
+    class BoxLocation
+    {
+        /// <summary>
+        /// ID клиента.
+        /// </summary>
+        public string id { get; set; }
+
+        /// <summary>
+        /// Долгота
+        /// </summary>
+        public double lon1 { get; set; }
+
+        /// <summary>
+        /// Широта
+        /// </summary>
+        public double lat1 { get; set; }
+
+        public double signal { get; set; }
+
+        public DateTime date { get; set; }
+    }
+}
